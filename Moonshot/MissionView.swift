f6127b2223337a6a9a551eb87cf6f722bd34f1cf
n0m4da <nomad._@hotmail.com>
2024-01-12 14:59:22 -0700

    mission view

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,46 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Luis Rivera on 12/01/24.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    //MARK: - PROPERTIES
+    
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView{
+            VStack{
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal){ width, axis  in width * 0.6
+                    }
+                
+                VStack(alignment:.leading){
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.black)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
+
